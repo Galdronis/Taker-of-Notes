7d53ef93823077879b3892f2e68f1b25446e74c7
Nicholas148894 <nich.oettinger@gmail.com>
2022-10-10 10:41:30 -0400

    trying to fix heroku deployment

--- server.js
+++ server.js
@@ -6,7 +6,6 @@ const PORT = process.env.PORT || 3001;
 const idgenerater = require('./idgenerater')
 app.use(express.urlencoded({extended: false}));
 app.use(express.json());
-app.use(express.static('public'));
 const {savednotes} = require('./Develop/db/db.json');
 
 const writenote = (body, savednotesArray) => {
