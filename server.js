e72b866b58d42f3f8b327fa8a6fa526f1e007d31
Nicholas148894 <nich.oettinger@gmail.com>
2022-10-11 14:01:50 -0400

    m

--- server.js
+++ server.js
@@ -20,11 +20,11 @@ const writenote = (body, savednotesArray) => {
   }; 
 
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, './public/index.html'));
+  res.sendFile(path.join(__dirname, './Develop/public/index.html'));
 });
 
 app.get('/notes', (req, res) => {
-  res.sendFile(path.join(__dirname, './public/notes.html'));
+  res.sendFile(path.join(__dirname, './Develop/public/notes.html'));
 });
 
 app.get('/api/notes', (req, res) => {
